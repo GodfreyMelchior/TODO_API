90eb5c7aa3dd55ad03d1840b27ec6f06f1c8e2fa
GodfreyMelchior <goddydev@gmail.com>
2026-07-04 18:24:10 +0300

    creating Todos view

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Todo
+from .serializers import TodoSerializer
 
 # Create your views here.
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    def get_queryset(self):
+        if self.action == 'list':
+            return Todo.objects.all() # Return only non-archived todos for the list action
+        return Todo.all_objects.all()     # Return all todos for other actions
